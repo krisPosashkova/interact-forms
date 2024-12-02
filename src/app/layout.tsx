7cd05a472f66d010a1e97d2c94c5c396aab51e43
Kristina Posashkova <106886437+krisPosashkova@users.noreply.github.com>
2024-12-03 01:39:33 +0400

    ad styled components and material ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,20 @@
 import type { Metadata } from "next";
+import { Inter, Zen_Dots } from "next/font/google";
+import AppThemeProvider from "@/providers/AppThemeProvider";
+
+const inter = Inter({
+    weight: ["400", "600"],
+    subsets: ["latin"],
+    variable: "--font-primary",
+    display: "swap",
+});
+
+const zen_dots = Zen_Dots({
+    weight: ["400"],
+    subsets: ["latin"],
+    variable: "--font-secondary",
+    display: "swap",
+});
 
 export const metadata: Metadata = {
     title: "Interact forms",
@@ -12,8 +28,10 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body>{children}</body>
+        <html lang="en" className={`${inter.variable} ${zen_dots.variable}`}>
+            <body>
+                <AppThemeProvider>{children}</AppThemeProvider>
+            </body>
         </html>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Container, Typography } from "@mui/material";
 export default function Home() {
-    return <div>Interact Form</div>;
+    return (
+        <Container maxWidth="xl">
+            <Typography component="h1" variant="h1">
+                Interact Forms
+            </Typography>
+        </Container>
+    );
 }
