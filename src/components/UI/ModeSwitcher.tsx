c831ff82bdc8cb943e48e358edee61c5d2e9d720
Kristina Posashkova <106886437+krisPosashkova@users.noreply.github.com>
2024-12-06 15:46:04 +0400

    rremove motion for mode switcher

--- src/components/UI/ModeSwitcher.tsx
+++ src/components/UI/ModeSwitcher.tsx
@@ -3,7 +3,6 @@
 import { useTheme } from "@/hooks/useMode";
 import { LightMode, DarkMode } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
-import { motion } from "framer-motion";
 
 const ModeSwitcher = () => {
     const { mode, toggleMode } = useTheme();
@@ -13,20 +12,14 @@ const ModeSwitcher = () => {
     }
 
     return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.3 }}>
-            <IconButton
-                type="button"
-                sx={{
-                    transition:
-                        "background-color var(--custom-durations-ms300)",
-                }}
-                onClick={toggleMode}>
-                {mode === "light" ? <DarkMode /> : <LightMode />}
-            </IconButton>
-        </motion.div>
+        <IconButton
+            type="button"
+            sx={{
+                transition: "background-color var(--custom-durations-ms300)",
+            }}
+            onClick={toggleMode}>
+            {mode === "light" ? <DarkMode /> : <LightMode />}
+        </IconButton>
     );
 };
 
