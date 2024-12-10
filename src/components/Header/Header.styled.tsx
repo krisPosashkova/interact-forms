30ec0dec00bee946291fedb27da069c124f89034
Kristina Posashkova <106886437+krisPosashkova@users.noreply.github.com>
2024-12-10 14:16:36 +0400

    remove boxshadow for header

--- src/components/Header/Header.styled.tsx
+++ src/components/Header/Header.styled.tsx
@@ -8,7 +8,6 @@ export const CustomHeader = styled.header(({ theme }) => ({
     left: 0,
     color: theme.vars.palette.text.primary,
     width: "100%",
-    boxShadow: "0 -2px 6px #5d5c5c",
     backgroundColor: theme.vars.palette.background.paper,
 
     ".logo": { whiteSpace: "nowrap" },
