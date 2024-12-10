eb623e6dcf4470c0f7ca26a4d8159be61164a0fd
Kristina Posashkova <106886437+krisPosashkova@users.noreply.github.com>
2024-12-10 14:18:22 +0400

    update pageLayout

--- src/components/Layout/PageLayout.tsx
+++ src/components/Layout/PageLayout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import Header from "@/components/Header/Header";
+import { Box } from "@mui/material";
 
 type Props = {
     children?: ReactNode;
@@ -9,7 +10,9 @@ export default function PageLayout({ children }: Props) {
     return (
         <>
             <Header />
-            {children}
+            <Box sx={{ height: "calc(100svh - 70px)", overflowY: "auto" }}>
+                {children}
+            </Box>
         </>
     );
 }
