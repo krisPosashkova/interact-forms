d276ff6ed7ea746f5839eb03826d4b2b0fb65c9f
Kristina Posashkova <106886437+krisPosashkova@users.noreply.github.com>
2024-12-11 16:03:37 +0400

    fix signup page

--- src/app/[locale]/signup/page.tsx
+++ src/app/[locale]/signup/page.tsx
@@ -2,8 +2,8 @@ import FormSignUp from "@/components/Forms/Auth/SignUp";
 import AuthLayout from "@/components/Layout/AuthLayout";
 import { useAuthProps } from "@/hooks/useAuthProps";
 
-export default function SignIn() {
-    const props = useAuthProps("SignIn");
+export default function SignUp() {
+    const props = useAuthProps("SignUp");
 
     return (
         <AuthLayout props={props.layout}>
